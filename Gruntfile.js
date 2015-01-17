bf84222855b6195d5d932a0a62773c631e73593a
Ryan Cox <rbc@Ryans-MacBook-Pro.local>
2015-01-17 15:41:46 -0500

    reorders page

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,12 +10,52 @@ module.exports = function (grunt) {
         files: {
             src: ['_site/*.html']
         }
+    },
+
+    imagemin:  {
+
+      png: {
+        options: {
+          optimizationLevel: 7
+        },
+        files: [
+          {
+            // Set to true to enable the following options…
+            expand: true,
+            // cwd is 'current working directory'
+            cwd: 'img/',
+            src: ['*.png'],
+            // Could also match cwd line above. i.e. project-directory/img/
+            dest: 'img/compressed/',
+            ext: '.png'
+          }
+        ]
+      },
+      jpg: {
+        options: {
+          progressive: true
+        },
+        files: [
+          {
+            // Set to true to enable the following options…
+            expand: true,
+            // cwd is 'current working directory'
+            cwd: 'img/',
+            src: ['*.jpg'],
+            // Could also match cwd. i.e. project-directory/img/
+            dest: 'img/compressed/',
+            ext: '.jpg'
+          }
+        ]
+      }
     }
 
   });
 
+  //Load Tasks
   grunt.loadNpmTasks('grunt-html-validation');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  grunt.registerTask('default', ['validation']);
+  grunt.registerTask('default', ['validation','imagemin']);
 
 };
